3cb584db1fc199c543a705f0d13e557194b15496
Chris Kimpton <chris@kimptoc.net>
2017-10-08 00:46:12 +0100

    continuously get fare updates while journey in progress, buttons for quick/fake moves when in manual mode

--- client/src/main/scala/client/ScalaJSMain.scala
+++ client/src/main/scala/client/ScalaJSMain.scala
@@ -1,40 +1,57 @@
 package client
 
 import org.scalajs.dom
-import org.scalajs.dom.{Event, Position, PositionError, html}
 import org.scalajs.dom.html.Input
-import org.scalajs.dom.raw.{Element, Geolocation}
+import org.scalajs.dom.raw.Element
+import org.scalajs.dom.{Event, Position, PositionError}
 
 import scala.scalajs.js.JSON
 //import shared.SharedMessages
 
-import scala.scalajs.js
-import dom.ext.Ajax
-import org.w3c.dom.html.HTMLButtonElement
-
+import org.scalajs.dom.ext.Ajax
+import scala.scalajs.js.timers.setInterval
 
 object ScalaJSMain {
+
+  var journeyUnderway:Boolean = false
+
   def main(args: Array[String]): Unit = {
 
     val main = dom.document.getElementById("main-app")
 
-    addButton(main, "Start Journey (auto via GPS)", "start_journey" ,true)
-    addButton(main, "Start Journey (manual)", "start_journey")
-    addButton(main, "End Journey", "end_journey")
-    addButton(main, "Update Journey", "location_update")
+    addButton(main, "Start Journey (auto via GPS)", Some("start_journey") ,enableGPS = true) { journeyUnderway = true}
+    main.appendChild(dom.document.createElement("br"))
+    addButton(main, "Start Journey (manual)", Some("start_journey")) { journeyUnderway = true}
+    main.appendChild(dom.document.createElement("p"))
+//    addButton(main, "Update Journey", Some("location_update")) {}  // ideally lambda should be option - possible in Scala?
+//    main.appendChild(dom.document.createElement("br"))
+    addButton(main, "Go to Oxford Circus", action = None) { setInputValue("latitude", 51.515419); setInputValue("longitude",-0.141099	)}  // ideally lambda should be option - possible in Scala?
+    addButton(main, "Go to Greenwich Park", action = None) { setInputValue("latitude", 51.47669); setInputValue("longitude",0.00013	)}  // ideally lambda should be option - possible in Scala?
+    main.appendChild(dom.document.createElement("p"))
+    addButton(main, "End Journey", Some("end_journey")) { journeyUnderway = false}
+    main.appendChild(dom.document.createElement("br"))
+
+
+    setInterval(100) {
+      println(s"backgrounder, journey underway? $journeyUnderway")
+      if (journeyUnderway) {
+        sendUpdate("location_update")
+      }
+    }
 
   }
 
-  private def addButton(main: Element, title: String, action: String, enableGPS : Boolean = false): Any = {
+  private def addButton(main: Element, title: String, action: Option[String], enableGPS : Boolean = false)(body: => Unit): Any = {
     val startButton = dom.document.createElement("button")
     startButton.textContent = title
     startButton.addEventListener("click", { (e0: Event) =>
+      body
       if (enableGPS) {
         // TODO how to detect if geolocation is available?
         var geo = dom.document.defaultView.navigator.geolocation
         def onSuccess(p:Position) = {
-          println( s"/latitude=${p.coords.latitude}")                // Latitude
-          println( s"/longitude=${p.coords.longitude}")              // Longitude
+//          println( s"/latitude=${p.coords.latitude}")                // Latitude
+//          println( s"/longitude=${p.coords.longitude}")              // Longitude
           setInputValue("latitude", p.coords.latitude)
           setInputValue("longitude", p.coords.longitude)
 
@@ -43,7 +60,9 @@ object ScalaJSMain {
         geo.getCurrentPosition(onSuccess _)
         geo.watchPosition(onSuccess _, onError _)
       }
-      sendUpdate(action)
+      if (action.nonEmpty) {
+        sendUpdate(action.get)
+      }
     }, false)
 
     main.appendChild(startButton)
@@ -51,10 +70,7 @@ object ScalaJSMain {
   }
 
   private def sendUpdate(action:String) = {
-    import scala.concurrent
-    .ExecutionContext
-    .Implicits
-    .global
+    import scala.concurrent.ExecutionContext.Implicits.global
 
     val latitude = inputValue("latitude")
     val longitude = inputValue("longitude")
@@ -63,13 +79,17 @@ object ScalaJSMain {
     val url = s"$backend_url/$action/$latitude/$longitude"
     Ajax.post(url).onComplete { case xhr =>
       println("ajax call complete")
-      println(xhr.get.responseText)
+//      println(xhr.get.responseText)
       val r = JSON.parse(xhr.get.responseText)
-      dom.document.getElementById("fare").textContent = r.fare.toString()
-      dom.document.getElementById("elapsed").textContent = r.elapsed.toString()
-      dom.document.getElementById("distance").textContent = r.distance.toString()
-      dom.document.getElementById("timestamp").textContent = r.timestamp.toString()
+      setText("fare", r.fare)
+      setText("elapsed",r.elapsed)
+      setText("distance",r.distance)
+      setText("timestamp",r.timestamp)
     }
+  }
+
+  private def setText(id: String, value: Any) = {
+    dom.document.getElementById(id).textContent = value.toString()
   }
 
   private def inputValue(id: String) = {
